fed6ed1e313e97cf0ef6d7f50923e7e2a199e9ea
vivekkumarbiswal <vivekkumarbiswal@gmail.com>
2026-04-07 14:02:53 +0530

    created UI and added getAllCars()

--- projects/api/src/app/post-api/post-api.component.ts
+++ projects/api/src/app/post-api/post-api.component.ts
@@ -1,11 +1,22 @@
+import { NgFor } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-post-api',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './post-api.component.html',
-  styleUrl: './post-api.component.css'
+  styleUrl: './post-api.component.css',
 })
 export class PostApiComponent {
+  carList: any[] = [];
+  constructor(private http: HttpClient) {}
 
+  getAllCars() {
+    this.http
+      .get('https://freeapi.miniprojectideas.com/api/CarRentalApp/GetCars')
+      .subscribe((res: any) => {
+        this.carList = res.data;
+      });
+  }
 }
